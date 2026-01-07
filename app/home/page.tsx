3125ce7202b4c98fe42bd62b105d564d416cdd1d
Hardik7219 <hendreogaming@gmail.com>
2026-01-07 22:50:41 +0530

    add profile and task page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faList, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
-
+import Task from '../task/page'
 import { useState } from "react";
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
                             <button onClick={()=>{
                                 setSideOption(true);
                             }}>{sideOptions ?  "" : <FontAwesomeIcon icon={faList} />}
-</button>
+                            </button>
                         </div>
                         {sideOptions && (
                             <div className="lg:hidden fixed block h-10" onClick={()=>{
@@ -35,26 +35,10 @@ export default function Home() {
                         <button className="h-14 w-28 bg-red-500 border rounded-sm
                         cursor-pointer text-2xl font-bold">Delete</button>
                     </aside>
-
-                <main className="bg-gray-900 flex justify-self-center lg:justify-self-end p-2 rounded-md w-[90%] min-h-screen">
-                    <div className="w-full p-1 lg:w-full bg-gray-800 flex flex-col items-center rounded-sm">
-                        <div className="bg-gray-900 mt-2 p-2 min-h-56 h-auto w-[95%]">
-                            <div className="w-full">
-                                <h1 className="font-bold text-cyan-800 text-lg">TITLE</h1>
-                                <div className="ml-5 flex justify-end p-1">
-                                    <input type="checkbox" className="h-8 w-8"></input>
-                                </div>
-                            </div>
-                            <div className=" p-1 bg-gray-500 mt-2 w-full min-h-30 h-auto text-wrap tracking-tighter overflow-hidden">
-                                <p className="font-mono ">L. Sed saepe possimus dicta quae sit pariatur provident quidem consequuntur, laborum porro digni</p>
-                            </div>
-                            <div className="mt-2 flex justify-end p-1">
-                                <button className="bg-green-300 mr-2 px-10 rounded-sm text-black font-bold text-2xl">EDIT</button>
-                                Complate:<input type="checkbox" className="h-8 w-8"></input>
-                            </div>
-                        </div>
+                    <div className='w-[90%] flex justify-self-center lg:justify-self-end p-2'>
+                        <Task></Task>
                     </div>
-                </main>
+
             </div>
         </>
     );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 import Link from "next/link";
 
 export default function Navbar() {
-    const [user,setUser]=useState<string>("SIGN IN");
+    const [user,setUser]=useState<boolean>(false);
+    const [isLogin,setIsLogin]=useState<boolean>(false);
     return (
         <>
           <div className="w-full h-15 lg:h-10 flex items-center justify-between bg-gray-800 p-3 sticky top-0 z-10 text-black">
             <div className="bg-black text-amber-50 justify-self-start "><Link href={"/home"}>APP</Link></div>
             <div className="justify-self-end">
-              <Link className=" hover:text-gray-800 bg-blue-100 p-1 m-1 rounded-[5px] text-red-600 " href="/sign">{user}</Link>
-              <Link className=" hover:text-gray-800 bg-blue-100 p-1 m-1 rounded-[5px] text-green-500 "href="/login">login</Link>
+              <Link className=" hover:text-gray-800 bg-blue-100 p-1 m-1 rounded-[5px] text-red-600 " href={user? "/logout" : "/signin"}>{user? "LOG OUT" : "SIGN IN"}</Link>
+              <Link className=" hover:text-gray-800 bg-blue-100 p-1 m-1 rounded-[5px] text-green-500 "href={isLogin? "/profile" : "/login"}>{isLogin? "PROFILE" : "LOG IN"}</Link>
             </div>
         </div>
         </>
