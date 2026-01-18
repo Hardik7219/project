b6bf8f5527ee0a5759838bf58ba3e551a5ba1881
Hardik7219 <hendreogaming@gmail.com>
2026-01-18 19:23:29 +0530

    change the z-index

--- components/addAchievement/ADD.tsx
+++ components/addAchievement/ADD.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { error } from "console";
 import Loading from "../LoadingScreen/Loading";
 
 export default function ADD() {
--- components/popup/Modal.tsx
+++ components/popup/Modal.tsx
@@ -9,7 +9,7 @@ export default function Modal({
 }) {
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center  backdrop-blur-sm"
+      className="fixed inset-0 z-40 flex items-center justify-center  backdrop-blur-sm"
       onClick={onClose}
     >
       <div
