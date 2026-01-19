19b26f0bbe558a407cc9ca5432023622ef0fbf4e
Hardik7219 <hendreogaming@gmail.com>
2026-01-19 08:33:57 +0530

    fix the star filter feature

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 
                     <aside className={`rounded-sm gap-2  flex flex-col  inset-0 z-20 top-0 fixed  justify-self-end self-end m-10 lg:m-20`}>
                         <div className='p-2 h-11 w-11 lg:h-28 lg:w-28 bg-black flex justify-center items-center rounded-full'>
-                            <button onClick={()=>setStar(true)} className="h-10 w-10 lg:h-24 lg:w-24 bg-zinc-900 border rounded-full border-s-indigo-50 cursor-pointer flex justify-center items-center text-3xl hover:border-indigo-600 transition-colors font-mono"><FontAwesomeIcon icon={faStar} className='text-yellow-400 hover:scale-115'/></button>
+                            <button onClick={()=>setStar(current => !current)} className="h-10 w-10 lg:h-24 lg:w-24 bg-zinc-900 border rounded-full border-s-indigo-50 cursor-pointer flex justify-center items-center text-3xl hover:border-indigo-600 transition-colors font-mono"><FontAwesomeIcon icon={faStar} className='text-yellow-400 hover:scale-115'/></button>
                         </div>
                         <div className='p-2 h-11 w-11 lg:h-28 lg:w-28 bg-black flex justify-center items-center rounded-full'>
                                 <button onClick={()=>showDate(true)} className="h-10 w-10 lg:h-24 lg:w-24 bg-zinc-900 border rounded-full border-s-indigo-50 cursor-pointer flex justify-center items-center text-3xl hover:border-indigo-600 transition-colors font-mono" ><FontAwesomeIcon icon={faCalendar}  className=' text-green-500 hover:text-green-300 hover:scale-120 '></FontAwesomeIcon></button>
